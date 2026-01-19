7aba81ce508ba05ada2aefc1d944e9d973a009e8
xtruong101 <truong13062k4@gmail.com>
2026-01-20 00:15:38 +0700

    test

--- utils.py
+++ utils.py
@@ -47,16 +47,16 @@ class DiceLoss(nn.Module):
 
 
 def calculate_metric_percase(pred, gt):
-#     pred[pred > 0] = 1
-#     gt[gt > 0] = 1
-#     if pred.sum() > 0 and gt.sum()>0:
-#         dice = metric.binary.dc(pred, gt)
-#         hd95 = metric.binary.hd95(pred, gt)
-#         return dice, hd95
-#     elif pred.sum() > 0 and gt.sum()==0:
-#         return 1, 0
-#     else:
-#         return 0, 0
+    pred[pred > 0] = 1
+    gt[gt > 0] = 1
+    if pred.sum() > 0 and gt.sum()>0:
+        dice = metric.binary.dc(pred, gt)
+        hd95 = metric.binary.hd95(pred, gt)
+        return dice, hd95
+    elif pred.sum() > 0 and gt.sum()==0:
+        return 1, 0
+    else:
+        return 0, 0
 
 
 # # def test_single_volume(image, label, net, classes, patch_size=[256, 256], test_save_path=None, case=None, z_spacing=1):
