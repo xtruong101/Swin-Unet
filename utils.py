86e3824dc1a4c28c81caa14f0c9da2e6570d6e86
xtruong101 <truong13062k4@gmail.com>
2026-01-21 00:32:50 +0700

    update

--- utils.py
+++ utils.py
@@ -59,7 +59,9 @@ def calculate_metric_percase(pred, gt):
 
 
 def test_single_volume(image, label, net, classes, patch_size=[256, 256], test_save_path=None, case=None, z_spacing=1):
-    image, label = image.squeeze(0).cpu().detach().numpy().squeeze(0), label.squeeze(0).cpu().detach().numpy().squeeze(0)
+    image = image.squeeze().cpu().detach().numpy()
+    label = label.squeeze().cpu().detach().numpy()
+    
     if len(image.shape) == 3:
         prediction = np.zeros_like(label)
         for ind in range(image.shape[0]):
