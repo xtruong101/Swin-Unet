ccfebdacb39d27d1a8c25241d378c9bb3f26627a
xtruong101 <truong13062k4@gmail.com>
2026-06-10 14:37:21 +0700

    fix

---
+++ train_acdc.py
@@ -0,0 +1,113 @@
+import argparse
+import logging
+import os
+import random
+import sys
+import numpy as np
+import torch
+import torch.backends.cudnn as cudnn
+from networks.vision_transformer import SwinUnet as ViT_seg
+from trainer_acdc import trainer_synapse, trainer_acdc
+from config import _C as config, _update_config_from_file
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--cfg', type=str, 
+                    default='./configs/swin_tiny_patch4_window7_224_lite.yaml', 
+                    help='path to config file', metavar="FILE")
+parser.add_argument('--opts', help="Modify config options by adding 'KEY VALUE' pairs. ", default=[], nargs='+')
+parser.add_argument('--output_dir', type=str,
+                    default='../model', help='output directory for model and log')
+parser.add_argument('--root_path', type=str,
+                    default='../data/Synapse/train_npz', help='root dir for data')
+parser.add_argument('--dataset', type=str,
+                    default='Synapse', help='experiment_name')
+parser.add_argument('--list_dir', type=str,
+                    default='./lists/lists_Synapse', help='list dir')
+parser.add_argument('--num_classes', type=int,
+                    default=9, help='output channel of network')
+parser.add_argument('--max_iterations', type=int,
+                    default=30000, help='maximum epoch number to train')
+parser.add_argument('--max_epochs', type=int,
+                    default=150, help='maximum epoch number to train')
+parser.add_argument('--batch_size', type=int,
+                    default=24, help='batch_size per gpu')
+parser.add_argument('--n_gpu', type=int, default=1, help='total gpu')
+parser.add_argument('--deterministic', type=int,  default=1,
+                    help='whether use deterministic training')
+parser.add_argument('--base_lr', type=float,  default=0.01,
+                    help='segmentation network learning rate')
+parser.add_argument('--img_size', type=int,
+                    default=224, help='input patch size of network input')
+parser.add_argument('--seed', type=int,
+                    default=1234, help='random seed')
+parser.add_argument('--n_skip', type=int,
+                    default=3, help='using number of skip-connect, default is num')
+args = parser.parse_args()
+
+
+def resolve_output_dir(output_dir: str | None) -> str:
+    if output_dir and output_dir != "../model":
+        return output_dir
+    if os.environ.get("KAGGLE_URL_BASE") or os.path.exists("/kaggle/working"):
+        return "/kaggle/working/model"
+    return "./model"
+
+
+if __name__ == "__main__":
+    # Load config
+    _update_config_from_file(config, args.cfg)
+    if args.opts:
+        config.defrost()
+        config.merge_from_list(args.opts)
+        config.freeze()
+    
+    if not args.deterministic:
+        cudnn.benchmark = True
+        cudnn.deterministic = False
+    else:
+        cudnn.benchmark = False
+        cudnn.deterministic = True
+
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    torch.cuda.manual_seed(args.seed)
+    dataset_name = args.dataset
+    dataset_config = {
+        'ACDC': {
+            'root_path': '../data/ACDC',
+            'list_dir': None,
+            'num_classes': 4,
+        },
+        'Synapse': {
+            'root_path': '../data/Synapse/train_npz',
+            'list_dir': './lists/lists_Synapse',
+            'num_classes': 9,
+        },
+    }
+    args.num_classes = dataset_config[dataset_name]['num_classes']
+    # Only override root_path if not explicitly provided via --root_path
+    if '--root_path' not in sys.argv:
+        args.root_path = dataset_config[dataset_name]['root_path']
+    if '--list_dir' not in sys.argv:
+        args.list_dir = dataset_config[dataset_name]['list_dir']
+    args.output_dir = resolve_output_dir(args.output_dir)
+    args.is_pretrain = True
+    args.exp = 'TU_' + dataset_name + str(args.img_size)
+    snapshot_path = "{}/{}/{}".format(args.output_dir, args.exp, 'TU')
+    snapshot_path = snapshot_path + '_pretrain' if args.is_pretrain else snapshot_path
+    snapshot_path = snapshot_path + '_skip' + str(args.n_skip)
+    snapshot_path = snapshot_path+'_'+str(args.max_iterations)[0:2]+'k' if args.max_iterations != 30000 else snapshot_path
+    snapshot_path = snapshot_path + '_epo' +str(args.max_epochs) if args.max_epochs != 30 else snapshot_path
+    snapshot_path = snapshot_path+'_bs'+str(args.batch_size)
+    snapshot_path = snapshot_path + '_lr' + str(args.base_lr) if args.base_lr != 0.01 else snapshot_path
+    snapshot_path = snapshot_path + '_'+str(args.img_size)
+    snapshot_path = snapshot_path + '_s'+str(args.seed) if args.seed!=1234 else snapshot_path
+
+    if not os.path.exists(snapshot_path):
+        os.makedirs(snapshot_path)
+    
+    net = ViT_seg(config, img_size=args.img_size, num_classes=args.num_classes).cuda()
+
+    trainer = {'Synapse': trainer_synapse,'ACDC': trainer_acdc,}
+    trainer[dataset_name](args, net, snapshot_path)
